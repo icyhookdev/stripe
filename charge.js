f04c21776ce6d2747c11c409f3b5c4139df12132
thienJs <cristian.albarran95@gmail.com>
2019-04-02 18:58:08 -0400

    finish build

---
+++ charge.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const stripe = require('stripe')('sk_test_yHTNEPsJXrwULmg8goBtQu6D');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  res.send('hi tehre');
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { status } = await stripe.charges.create({
+      amount: req.body.amount,
+      currency: 'usd',
+      description: 'An example charge',
+      source: req.body.token.id,
+    });
+    console.log(status);
+    res.json({ status });
+    return true;
+  } catch (err) {
+    res.status(500).end();
+    console.log(err);
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,42 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
-const keys = require('./config/keys_prod');
-const stripe = require('stripe')(keys.stripeSecretKey);
+const charge = require('./charge');
 
 const app = express();
+app.use(require('body-parser').text());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/', (req, res) => {
-  res.render('index', {
-    stripePublishableKey: keys.stripePublishableKey,
-  });
-});
-
-app.post('/charge', (req, res) => {
-  const { amount } = req.body;
-
-  stripe.customers
-    .create({
-      email: req.body.stripeEmail,
-      source: req.body.stripeToken,
-    })
-    .then(customer =>
-      stripe.charges.create({
-        amount,
-        description: 'Web Development Ebook',
-        currency: 'usd',
-        customer: customer.id,
-      })
-    )
-    .then(charge => console.log('works'));
-});
+app.use(cors());
+app.use(express.json());
+app.use('/charge', charge);
 
 const port = process.env.PORT || 5000;
-app.use(cors());
-
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
