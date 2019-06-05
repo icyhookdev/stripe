f7ed516ea992ce449f93a3efdbf65daaecef5d5c
thien <cristian.albarran95@gmail.com>
2019-06-05 11:44:30 -0400

    creating a test endpoint for try stripe payments

---
+++ chargetest.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY2;
+const stripe = require('stripe')(stripeSecretKey);
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  res.send('hi tehre');
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { status } = await stripe.charges.create({
+      amount: req.body.amount,
+      currency: 'usd',
+      description: 'An example charge',
+      source: req.body.token.id,
+    });
+    console.log(status);
+    res.json({ status });
+    return true;
+  } catch (err) {
+    res.status(500).end();
+
+    console.log(err);
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const charge = require('./charge');
 const email = require('./email');
+const chargetest = require('./chargetest');
 require('dotenv').config();
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/charge', charge);
 app.use('/email', email);
+app.use('/chargetest', chargetest);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
